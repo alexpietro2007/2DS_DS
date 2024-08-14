44a471b55096a42b7c4f9a008ab424413310bcc3
alexPietro <alexpieetro@gmail.com>
2024-08-14 06:58:04 -0300

    Atualizar o PrismaPentagonal.java

--- formas/src/PrismaPentagonal.java
+++ formas/src/PrismaPentagonal.java
@@ -1,7 +1,7 @@
 public class PrismaPentagonal extends Pentagono{
     @Override
     public void calcular(){
-        super.setResul(super.getAltura() * Math.pow(super.getLado(), 5)*super.getComprimento());
+        super.setResul(super.getAltura() * super.getLado()*super.getComprimento());
     };
 
     @Override
