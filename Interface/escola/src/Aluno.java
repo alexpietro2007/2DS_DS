23e3ce6a7efabc8e3fe0285121728f7c955dffc5
alexPietro <alexpieetro@gmail.com>
2024-09-25 08:38:30 -0300

    Interface

---
+++ Interface/escola/src/Aluno.java
@@ -0,0 +1,51 @@
+
+public class Aluno implements Pessoa {
+
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String matricula;
+
+    public Aluno(String nome, String endereco, String telefone, String matricula) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.matricula = matricula;
+
+    }
+// Getters
+    @Override
+    public String getNome() {
+        return this.nome;
+    }
+    @Override
+    public String getEndereco() {
+        return this.endereco;
+
+    }
+    @Override
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public String getMatricula() {
+        return this.matricula;
+    }
+
+    @Override
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    @Override
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    @Override
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+
+
+}
